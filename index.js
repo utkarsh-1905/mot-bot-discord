b1d077983a60c51a4d80509d3444e21e0088d3c5
Utkarsh Tripathi <utripathi2002@gmail.com>
2022-02-04 23:42:37 +0530

    Basic reddit api fetching bot ready

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
+
+// https://meme-api.herokuapp.com/gimme API
 
 const { Client, Intents } = require("discord.js");
+const fetch = require("node-fetch");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-const fetch = require("node-fetch");
 
-client.once('ready',()=>{
-        console.log('ready');
-})
+const getRedditData = async (subreddit) => {
+  let data;
+  if (subreddit.length > 2)
+    data = await fetch(`https://meme-api.herokuapp.com/gimme/${subreddit}/1`);
+  else data = await fetch(`https://meme-api.herokuapp.com/gimme/1`);
+  const res = await data.json();
+  return res;
+};
 
-client.on("messageCreate", (message) => {
-  if(message.author.bot) return;
-  if(!message.content.startsWith('^')) return;
-  if(message.content === "!ping"){
-          message.reply(`Hello ${message.author.username}`);
+client.once("ready", () => {
+  console.log("ready");
+});
+
+client.on("messageCreate", async (message) => {
+  // console.log(message);
+  if (message.author.bot) return;
+  if (!message.content.startsWith("^")) return;
+  if (message.content === "^ping") {
+    message.reply(`Hello ${message.author.username}`);
+  } else if (message.content === "^help") {
+    message.reply(helpReply);
+  } else if (message.content === "^meme") {
+    const subreddit = await getRedditData(" ");
+    // console.log(subreddit);
+    let url = subreddit.memes[0].preview[3];
+    message.channel.send(url);
+  } else if (message.content.startsWith("^")) {
+    const subreddit = await getRedditData(message.content.slice(1));
+    if (subreddit.code === 404) {
+      message.reply(subreddit.message);
+    } else {
+      let url = subreddit.memes[0].preview[3];
+      message.channel.send(url);
+    }
   }
 });
 
 client.login(process.env.TOKEN);
+
+const helpReply =
+  "```Prepend all request with ^\nhelp:Will help you\nmeme:Will give you a meme\n<any subreddit page>: Will return a post from that subreddit```";
